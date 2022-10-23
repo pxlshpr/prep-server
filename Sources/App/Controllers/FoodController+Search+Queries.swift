1c10849b176f55908c68f35cfb3fd49739e0d649
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-23 15:59:53 +0500

    bump

--- Sources/App/Controllers/FoodController+Search+Queries.swift
+++ Sources/App/Controllers/FoodController+Search+Queries.swift
@@ -12,9 +12,21 @@ func queryDetailPrefix(string: String, db: Database) -> QueryBuilder<Food> {
         .filter(\.$detail, .custom("ILIKE"), "\(string)%")
 }
 
+func queryBrandPrefix(string: String, db: Database) -> QueryBuilder<Food> {
+    Food.query(on: db)
+        .filter(\.$brand, .custom("ILIKE"), "\(string)%")
+}
+
+func queryNameWithDetailPrefix(string: String, db: Database) -> QueryBuilder<Food> {
+    Food.query(on: db)
+        .filter(.sql(raw: "CONCAT(name,' ',detail) ILIKE '\(string)%'"))
+}
+
 func queries(string: String, db: Database) -> [String: QueryBuilder<Food>] {
     [
         "NamePrefix": queryNamePrefix(string: string, db: db),
-        "DetailPrefix": queryDetailPrefix(string: string, db: db)
+        "DetailPrefix": queryDetailPrefix(string: string, db: db),
+        "BrandPrefix": queryBrandPrefix(string: string, db: db),
+        "NameWithDetailPrefix": queryNameWithDetailPrefix(string: string, db: db),
     ]
 }
