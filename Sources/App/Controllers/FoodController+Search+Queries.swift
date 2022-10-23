9317ac0f22df99e249f803b6b51b40583a1463fc
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-23 18:43:45 +0500

    bump

--- Sources/App/Controllers/FoodController+Search+Queries.swift
+++ Sources/App/Controllers/FoodController+Search+Queries.swift
@@ -6,21 +6,53 @@ import PrepUnits
 func queries(string: String, db: Database) -> [(String, QueryBuilder<Food>)] {
     [
         ("Name Match", queryNameMatch(string: string, db: db)),
-        ("Name Component Match (All Components)", queryNameComponent(string: string, db: db, strictMatch: true, joinOperator: .and)),
-        ("Name Component Contains (All Components)", queryNameComponent(string: string, db: db, strictMatch: false, joinOperator: .and)),
-        
-        ("Name Component Match (Any Component)", queryNameComponent(string: string, db: db, strictMatch: true, joinOperator: .or)),
-        ("Name Component Contains (Any Component)", queryNameComponent(string: string, db: db, strictMatch: false, joinOperator: .or)),
+        ("Name Component Match (All Components)", queryComponents(on: .name, string: string, db: db, strictMatch: true, joinOperator: .and)),
+        ("Name Component Contains (All Components)", queryComponents(on: .name, string: string, db: db, strictMatch: false, joinOperator: .and)),
+
+        ("Name and Detail Component Match (All Components)", queryComponents(on: .nameAndDetail, string: string, db: db, strictMatch: true, joinOperator: .and)),
+        ("Name and Detail Component Contains (All Components)", queryComponents(on: .nameAndDetail, string: string, db: db, strictMatch: false, joinOperator: .and)),
+
+        ("Name Component Match (Any Component)", queryComponents(on: .name, string: string, db: db, strictMatch: true, joinOperator: .or)),
+        ("Name Component Contains (Any Component)", queryComponents(on: .name, string: string, db: db, strictMatch: false, joinOperator: .or)),
         ("Fuzzy Contains (All Components)", fuzzyComponentContains(string: string, db: db, joinOperator: .and)),
         ("Fuzzy Contains (Any Component)", fuzzyComponentContains(string: string, db: db, joinOperator: .or)),
     ]
 }
 
 
-func queryNameComponent(string: String, db: Database, strictMatch: Bool, joinOperator: JoinOperator) -> QueryBuilder<Food> {
+func queryComponents(on fields: DetailFields, string: String, db: Database, strictMatch: Bool, joinOperator: JoinOperator) -> QueryBuilder<Food> {
     Food.query(on: db)
-        .filter(.sql(raw: string.whereClauseForNameComponent(strictMatch: strictMatch, joinOperator: joinOperator)))
+        .filter(.sql(raw: string.whereClauseForComponents(fields: fields, strictMatch: strictMatch, joinOperator: joinOperator)))
         .sort(.sql(raw: "CHAR_LENGTH(CONCAT(name,' ',detail,' ',brand))-CHAR_LENGTH('\(string)')"))
+}
+
+enum DetailFields {
+    case name
+    case detail
+    case brand
+    case nameAndDetail
+    case nameAndBrand
+    case detailAndBrand
+    case all3
+    
+    var descriptor: String {
+        switch self {
+        case .name:
+            return "name"
+        case .detail:
+            return "detail"
+        case .brand:
+            return "brand"
+        case .nameAndDetail:
+            return "CONCAT(name,' ',detail)"
+        case .nameAndBrand:
+            return "CONCAT(name,' ',brand)"
+        case .detailAndBrand:
+            return "CONCAT(detail,' ',brand)"
+        case .all3:
+            return "CONCAT(name,' ',detail,' ',brand)"
+        }
+    }
 }
 
 extension String {
@@ -29,11 +61,11 @@ extension String {
      AND CONCAT(name,' ',detail,' ',brand) ILIKE '%breast%'
      AND CONCAT(name,' ',detail,' ',brand) ILIKE '%raw%'
      */
-    func whereClauseFuzzyComponent(joinOperator: JoinOperator) -> String {
-        components(separatedBy: " ")
-        .map { "CONCAT(name,' ',detail,' ',brand) ILIKE '%\($0)%'" }
-        .joined(separator: " \(joinOperator.separator) ")
-    }
+//    func whereClauseFuzzyComponent(joinOperator: JoinOperator) -> String {
+//        components(separatedBy: " ")
+//        .map { "CONCAT(name,' ',detail,' ',brand) ILIKE '%\($0)%'" }
+//        .joined(separator: " \(joinOperator.separator) ")
+//    }
     
     /**
      name ILIKE 'chicken breaks'
@@ -42,13 +74,19 @@ extension String {
         "name ILIKE '\(self)'"
     }
     
-    /**
-     name ILIKE 'chicken'
-     OR name ILIKE 'breast'
-     */
-    func whereClauseForNameComponent(strictMatch: Bool, joinOperator: JoinOperator) -> String {
+//    /**
+//     name ILIKE 'chicken'
+//     OR name ILIKE 'breast'
+//     */
+//    func whereClauseForNameComponent(strictMatch: Bool, joinOperator: JoinOperator) -> String {
+//        components(separatedBy: " ")
+//            .map { "name ILIKE '\(!strictMatch ? "%" : "")\($0)\(!strictMatch ? "%" : "")'" }
+//            .joined(separator: " \(joinOperator.separator) ")
+//    }
+    
+    func whereClauseForComponents(fields: DetailFields, strictMatch: Bool, joinOperator: JoinOperator) -> String {
         components(separatedBy: " ")
-            .map { "name ILIKE '\(!strictMatch ? "%" : "")\($0)\(!strictMatch ? "%" : "")'" }
+            .map { "\(fields.descriptor) ILIKE '\(!strictMatch ? "%" : "")\($0)\(!strictMatch ? "%" : "")'" }
             .joined(separator: " \(joinOperator.separator) ")
     }
 
@@ -89,7 +127,7 @@ func queryNameMatch(string: String, db: Database) -> QueryBuilder<Food> {
  */
 func fuzzyComponentContains(string: String, db: Database, joinOperator: JoinOperator) -> QueryBuilder<Food> {
     Food.query(on: db)
-        .filter(.sql(raw: string.whereClauseFuzzyComponent(joinOperator: joinOperator)))
+        .filter(.sql(raw: string.whereClauseForComponents(fields: .all3, strictMatch: false, joinOperator: joinOperator)))
         .sort(.sql(raw: "CHAR_LENGTH(CONCAT(name,' ',detail,' ',brand))-CHAR_LENGTH('\(string)')"))
 }
 
